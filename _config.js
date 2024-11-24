ca3496e583d71e7febca4ac2f2d27f5576eedbd0
Raddames Tonui <raddamestonui48@gmail.com>
2024-11-24 21:20:05 +0300

    added mongo db

--- _config.js
+++ _config.js
@@ -2,8 +2,9 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://raddames:raddames123@cluster0.b6r82.mongodb.net/darkroom?retryWrites=true&w=majority',
+    development: 'mongodb+srv://raddames:raddames123@cluster0.b6r82.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+    test: 'mongodb+srv://raddames:raddames123@cluster0.b6r82.mongodb.net/darkroom-test?retryWrites=true&w=majority',
 }
+
 module.exports = config;
